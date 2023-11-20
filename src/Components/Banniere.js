cdac5f0e3fc11114a5be8d2ad9aa747bab35b520
marie57412 <mariebraun1@outlook.fr>
2023-11-20 11:17:53 +0100

    ajout border texte

--- src/Components/Banniere.js
+++ src/Components/Banniere.js
@@ -4,10 +4,12 @@ import React from "react";
 function Banniere() {
     return (
         <section className="section_banniere">
+            <div className="cadre-text">
             <p className="banniere_p">"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Sed vitae turpis ac velit pulvinar varius. Nulla facilisi.
                 Proin vel purus vitae nulla laoreet malesuada in id nisi."
             </p>
+            </div>
             <img src={"##"} alt="image de ma tete"></img>
         </section>
     )
