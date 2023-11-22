7a62b917b5ae4f5cefe4428ac2d5df9ae4b69be9
marie57412 <mariebraun1@outlook.fr>
2023-11-22 22:21:55 +0100

    rajout feuille de palmier

--- src/Components/Banniere.js
+++ src/Components/Banniere.js
@@ -2,9 +2,11 @@ import React from "react";
 import Avatar from "../assets/Avatar.png"
 
 
+
 function Banniere() {
     return (
         <section className="section_banniere">
+            <div className="texte-avatar">
             <div className="cadre-text">
             <p className="banniere_p">"Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Sed vitae turpis ac velit pulvinar varius. Nulla facilisi.
@@ -12,6 +14,7 @@ function Banniere() {
             </p>
             </div>
             <img src={Avatar} alt="image de ma tete"></img>
+            </div>
         </section>
     )
 }
