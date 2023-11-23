4826f6aec4bf0cd9cceb111597ef0ed432cd2422
marie57412 <mariebraun1@outlook.fr>
2023-11-23 23:25:51 +0100

    ajout des project avec l'api

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 function Project(){
+
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    const token = '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';
+    const headers = {
+      Authorization: `token ${token}`,
+      Accept: 'application/vnd.github.v3+json',
+    };
+
+    fetch('https://api.github.com/user/repos', { headers })
+      .then((response) => response.json())
+      .then((data) => {
+        setRepos(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
     return(
-       <div></div>
+       <section className='project'>
+        <h2>Mes projets</h2>
+        <ul>
+        {repos.map((repo) => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+       </section>
     )
 }
 
