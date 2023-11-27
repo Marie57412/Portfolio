cec32b1690599fe8ab0508ab31aa63c9a5128e2c
marie57412 <mariebraun1@outlook.fr>
2023-11-27 13:35:20 +0100

    affachige image par projet

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
+
 function ProjectCard({ repo }) {
   return (
     <div className="card">
-         <img src={repo.imageURL} alt={repo.name} />
+      <img className='imgProject' src={repo.image} alt={repo.name} />
       <h3>{repo.name}</h3>
       <p>{repo.description}</p>
       <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
         Voir sur GitHub
       </a>
-   
     </div>
   );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import ProjectCard from './Card';
+import ProjectCard from "../Components/Card"
+import ImageProject1 from "../assets/Images Projets/1toa-heftiba-DQKerTsQwi0-unsplash1.jpg";
+import ImageProject2 from "../assets/Images Projets/2sophie-bluel1689172633206.png";
+import ImageProject3 from "../assets/Images Projets/3AboutBanner.jpg";
+import ImageProject4 from "../assets/Images Projets/4nina.webp";
+import ImageProject5 from "../assets/Images Projets/5product-school-dJICd7b_LlE-unsplash.png";
+import ImageProject6 from "../assets/Images Projets/bank-tree.jpeg";
+import ImageProject7 from "../assets/Images Projets/portfolioImg.png";
 
 function Project(){
 
-  const [repos, setRepos] = useState([]);
+  
+  const [reposWithImages, setReposWithImages] = useState([]);
 
   useEffect(() => {
-    const token = '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';
+    const token = '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';
     const query = `
       query {
         viewer {
@@ -30,21 +38,63 @@ function Project(){
       },
       body: JSON.stringify({ query }),
     })
-      .then((response) => response.json())
-      .then((data) => {
-        setRepos(data.data.viewer.repositories.nodes);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+    .then((response) => response.json())
+    .then((data) => {
+      const repositories = data.data.viewer.repositories.nodes;
+
+      // Associe chaque repository avec son image correspondante
+      const reposMappedWithImages = repositories.map((repo, index) => ({
+        id: repo.id,
+        name: repo.name,
+        description: repo.description,
+        image: getCorrespondingImage(index), // Obtient l'image correspondante
+      }));
+
+      setReposWithImages(reposMappedWithImages);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+}, []);
+
+  const dataWithImages = [
+    { id: 1,  image: ImageProject1 },
+    { id: 2,  image: ImageProject2 },
+    { id: 3,  image: ImageProject3 },
+    { id: 4,  image: ImageProject4 },
+    { id: 5,  image: ImageProject5 },
+    { id: 6,  image: ImageProject6 }, 
+    { id: 7,  image: ImageProject7 },
+    // Ajoute d'autres objets avec les images correspondantes
+  ];
+  const getCorrespondingImage = (index) => {
+    switch (index) {
+      case 0:
+        return ImageProject1;
+      case 1:
+        return ImageProject2;
+      case 2:
+        return ImageProject3;
+      case 3:
+        return ImageProject4;
+      case 4:
+        return ImageProject5;
+      case 5:
+        return ImageProject6;
+      case 6:
+        return ImageProject7;
+      default:
+        return null;
+    }
+  };
 
   
+
     return(
-        <section className='project'>
+      <section className='project'>
       <h2>Mes projets</h2>
       <div className="card-container">
-        {repos.map((repo) => (
+        {reposWithImages.map((repo) => (
           <ProjectCard key={repo.id} repo={repo} />
         ))}
       </div>
