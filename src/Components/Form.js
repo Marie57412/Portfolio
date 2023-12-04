6d4e10788ebe0543f946f164f24983479269b846
marie57412 <mariebraun1@outlook.fr>
2023-12-04 11:26:15 +0100

    ajout veuillez remplir les champs

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -7,12 +7,20 @@ function EmailForm() {
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_xmlr0jp', 'template_tg7po18', e.target, 'aNH5b2MNMTv4eVQEk')
-      .then((result) => {
-        setSuccessMessage('Votre e-mail a bien été envoyé !');
-      }, (error) => {
-        console.log(error.text);
-      });
+
+      const { user_name, user_email, message } = e.target.elements;
+
+      if (user_name.value && user_email.value && message.value) {
+        emailjs.sendForm('service_xmlr0jp', 'template_tg7po18', e.target, 'aNH5b2MNMTv4eVQEk')
+          .then((result) => {
+            setSuccessMessage('Votre e-mail a bien été envoyé !');
+          }, (error) => {
+            console.log(error.text);
+          });
+      } else {
+        console.log('Veuillez remplir tous les champs.');
+        
+      }
   }
 
   return (
@@ -21,15 +29,15 @@ function EmailForm() {
       <form onSubmit={sendEmail}>
         <div>
           <label htmlFor="user_name">Nom :</label>
-          <input type="text" name="user_name" id="user_name" placeholder='Veuillez saisir votre nom' />
+          <input type="text" name="user_name" id="user_name" placeholder='Veuillez saisir votre nom' required />
         </div>
         <div>
           <label htmlFor="user_email">Email :</label>
-          <input type="email" name="user_email" id="user_email" placeholder='Veuillez saisir votre email' />
+          <input type="email" name="user_email" id="user_email" placeholder='Veuillez saisir votre email' required />
         </div>
         <div>
           <label htmlFor="message">Message :</label>
-          <textarea name="message" id="message" placeholder='Saisissez votre message'></textarea>
+          <textarea name="message" id="message" placeholder='Saisissez votre message' required></textarea>
         </div>
         <button type="submit">Envoyer</button>
       </form>
